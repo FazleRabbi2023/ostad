0c1815d1df52d9d133388f41fb9a428212f68c93
Fazle Rabbi <fazlerabbi@192.168.1.8>
2023-08-06 14:46:00 +0600

    second commit

--- src/App copy.vue
+++ src/App copy.vue
@@ -5,16 +5,16 @@
         computed
 } from 'vue'
 
-// import axios from 'axios';
+import axios from 'axios';
 
-// import credentials from './credentials/credentials'
+import credentials from './credentials/credentials'
 
 const activeClass = ref('login');
 let pass1 = ref('');
 let pass2 = ref('');
-const credentials = reactive([]);
 
 async function checkRegistration(p1, p2, u) {
+  activeClass.value = (p1 == p2) ? ((p1 == '' && p2 == '') ? 'emptypass' : (u == '' ? 'emptyusername' : '')) : 'missmatch';
 
   if (p1 == p2) {
     if (p1 == '' && p2 == '') {
@@ -24,20 +24,44 @@ async function checkRegistration(p1, p2, u) {
     } else if (credentials.find(c => c.username === u)) {
       activeClass.value = 'notUnique';
     } else {
-      credentials.push({ username: u, password: p1 });
-      activeClass.value = 'logout';
+      //credentials.push({ username: u, password: p1 });
+        
+        try {
+          const response = await axios.post('http://localhost:3001/credentials', {
+            username: u, password: p1
+          });
+          console.log('New post added:', response.data);
+          activeClass.value = 'logout';
+        } catch (error) {
+          console.error('Error adding new post:', error);
+        }
+      
+      
     }
   } else {
     activeClass.value='missmatch'
   }
   console.log(credentials);
-  
+  // const checkUniqueUser = credentials.find(c => c.name === u);
+  // if (!checkUniqueUser) {
+  //     credentials.push({
+  //     username: u,
+  //     password: p1
+  //     })
+  //   console.log(credentials);
+  //   activeClass.value = 'logout';
+  // } else {
+  //   activeClass.value = 'notUnique';
+  // }
 }
 
-function checkLogin(p, u) {
-  
-  const checkUser = credentials.find(user => user.username === u);
-  const checkPass = credentials.find(user => user.password === p);
+async function checkLogin(p, u) {
+  const response = await axios.get(
+          `http://localhost:3001/credentials`
+  );
+  //console.log(response.data);
+  const checkUser = response.data.find(user => user.username === u);
+  const checkPass = response.data.find(user => user.password === p);
   if (checkUser && checkPass) {
     activeClass.value = 'logout';
     let user = checkUser.username;
@@ -53,8 +77,7 @@ function checkLogin(p, u) {
     <section class="flex h-screen w-full">
         <div class="w-1/2"
             style="background-image: url(https://images.unsplash.com/photo-1690555575753-7aa27df96b52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80); background-repeat: no-repeat; background-size: cover;">
-            <h1 class="mb-5 text-2xl mt-10 ml-10 text-white">Kosmos! </h1>
-            
+            <h1 class="mb-5 text-2xl mt-10 ml-10 text-white">Kosmos! {{ activeClass }}</h1>
         </div>
         <div v-if="'login'==activeClass || 'notMatched'==activeClass" class="w-1/2 flex flex-col justify-center items-center bg-gray-200">
             
@@ -153,7 +176,6 @@ function checkLogin(p, u) {
         </div>
         <template v-if="'logout' == activeClass">
               <div class="w-1/2 flex flex-col justify-center items-center bg-gray-200">
-                <div class="p-4 mb-4 bg-white rounded">Welcome</div>
                 <h1>Hi {{ user }}, you have Logged in !!</h1>
                 <button @click="activeClass='login'" class="text-white mt-3 px-6 py-3 bg-red-500 hover:bg-red-700 rounded font-bold focus:outline-none focus:shadow-outline">Log Out</button>
               </div>
