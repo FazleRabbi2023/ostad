9d69d3de959b92d9ca6395981c79e2fa8d1ba6ef
Fazle Rabbi <fazlerabbi@Fazles-MacBook-Pro.local>
2023-08-07 00:16:16 +0600

    updated

---
+++ src/App 3.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+const inputValue = ref('Hello');
+
+
+</script>
+
+<template>
+    
+    <div class="mt-64 w-full flex flex-col items-center justify-center ">
+        <p class="mb-6 text-4xl text-bold text-red-500">{{ inputValue }}</p>
+        <input v-model="inputValue" class="text-2xl w-1/2 border-2 border-red-300 p-4 text-center rounded-lg" type="text" placeholder="value" />
+    </div>
+</template>
+
--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ let pass1 = ref('');
 let pass2 = ref('');
 const credentials = reactive([]);
 
-async function checkRegistration(p1, p2, u) {
+async function checkRegistration(p1, p2, u, f) {
 
   if (p1 == p2) {
     if (p1 == '' && p2 == '') {
@@ -20,7 +20,7 @@ async function checkRegistration(p1, p2, u) {
     } else if (credentials.find(c => c.username === u)) {
       activeClass.value = 'notUnique';
     } else {
-      credentials.push({ username: u, password: p1 });
+      credentials.push({ username: u, password: p1, fullname: f });
       activeClass.value = 'logout';
     }
   } else {
@@ -36,7 +36,8 @@ function checkLogin(p, u) {
   const checkPass = credentials.find(user => user.password === p);
   if (checkUser && checkPass) {
     activeClass.value = 'logout';
-    let user = checkUser.username;
+      let user = ref(checkUser.username);
+      let fullname = ref(checkUser.fullname);
   } else {
     activeClass.value = 'notMatched';
   }
@@ -46,11 +47,10 @@ function checkLogin(p, u) {
 </script>
 
 <template>
-    <section class="flex h-screen w-full">
+    <section v-if="'logout'!=activeClass" class="flex h-screen w-full">
         <div class="w-1/2"
             style="background-image: url(https://images.unsplash.com/photo-1690555575753-7aa27df96b52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80); background-repeat: no-repeat; background-size: cover;">
-            <h1 class="mb-5 text-2xl mt-10 ml-10 text-white">Kosmos! </h1>
-            
+            <h1 class="mb-5 text-2xl mt-10 ml-10 text-white"></h1>
         </div>
         <div v-if="'login'==activeClass || 'notMatched'==activeClass" class="w-1/2 flex flex-col justify-center items-center bg-gray-200">
             
@@ -103,6 +103,14 @@ function checkLogin(p, u) {
                     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                         <div class="mb-4">
                             <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                                Fullname
+                            </label>
+                            <input v-model="fullname"
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="username" type="text" placeholder="Username">
+                        </div>
+                        <div class="mb-4">
+                            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
                                 Username
                             </label>
                             <input v-model="username"
@@ -128,7 +136,7 @@ function checkLogin(p, u) {
                             <!-- <p class="text-red-500 text-xs italic">Please choose a password.</p> -->
                         </div>
                         <div class="flex items-center justify-between">
-                            <button @click="checkRegistration(pass1,pass2,username)"
+                            <button @click="checkRegistration(pass1,pass2,username,fullname)"
                                 class="bg-orange-600 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                                 type="button">
                                 Register
@@ -145,13 +153,20 @@ function checkLogin(p, u) {
             
             
         </div>
-        <template v-if="'logout' == activeClass">
+        <!-- <template v-if="'logout' == activeClass">
               <div class="w-1/2 flex flex-col justify-center items-center bg-gray-200">
                 <div class="p-4 mb-4 bg-white rounded">Welcome</div>
                 <h1>Hi {{ user }}, you have Logged in !!</h1>
                 <button @click="activeClass='login'" class="text-white mt-3 px-6 py-3 bg-red-500 hover:bg-red-700 rounded font-bold focus:outline-none focus:shadow-outline">Log Out</button>
               </div>
-        </template>
+        </template> -->
+    </section>
+    <section v-if="'logout' == activeClass" class="flex h-screen w-full content-center">
+        <div class="w-full flex flex-col items-center justify-center "
+            style="background-image: url(https://cdn.pixabay.com/photo/2016/02/13/12/26/aurora-1197753_1280.jpg); background-repeat: no-repeat; background-size: cover;">
+            <h1 class="mb-5 text-2xl mt-10 ml-10 text-white text-center">Welcome {{ fullname }} !!, you have Logged in.</h1>
+            <button @click="activeClass='login'" class="text-center text-white mt-3 px-6 py-3 bg-red-500 hover:bg-red-700 rounded font-bold focus:outline-none focus:shadow-outline">Log Out</button>
+        </div>
     </section>
 </template>
 <style scoped>
